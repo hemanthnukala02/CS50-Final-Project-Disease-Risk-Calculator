0233cdda89aba8d3b69af4cd6105ea76b97b5469
Hemanth Nukala <hemanthmurali04@gmail.com>
2024-12-15 16:12:37 +0800

    Integrated all 4 models into my html. Settled model.py and routes.py changes to allow for integration of data models. Created results.html to print out user results. Also removed height and weight from being encoded to improve model efficiency and logic. Added some style.css to beautify the website for index.html. Still need to beautify results.html.

--- app/model.py
+++ app/model.py
@@ -1,9 +1,37 @@
 import joblib
 import pandas as pd
 
+modelA = joblib.load('models/random_forest_model_A.pkl')
+modelC = joblib.load('models/random_forest_model_C.pkl')
+modelD = joblib.load('models/random_forest_model_D.pkl')
+modelHD = joblib.load('models/random_forest_model_HD.pkl')
+encoded_columns = joblib.load('models/encoded_columns.pkl')
+
 def predict_disease_risk(user_data):
     user_data_df = pd.DataFrame([user_data])
-    user_data_encoded = pd.get_dummies(user_data_df, columns['Exercise', 'Sex', 'Age_Category', 'Height_(cm)', 'Weight_(kg)', 'Smoking_History', "Alcohol Consumption"])
+    user_data_encoded = pd.get_dummies(user_data_df, columns=['Exercise', 'Sex', 'Age_Category', 'Smoking_History', "Alcohol Consumption"])
+
+    missing_cols = set(encoded_columns) - set(user_data_encoded)
+    for col in missing_cols:
+        user_data_encoded[col] = False
+
+    user_data_encoded = user_data_encoded[encoded_columns]
+
+    risk_percentage_A = modelA.predict_proba(user_data_encoded)[0][1] * 100
+    risk_percentage_C = modelC.predict_proba(user_data_encoded)[0][1] * 100
+    risk_percentage_D = modelD.predict_proba(user_data_encoded)[0][1] * 100
+    risk_percentage_HD = modelHD.predict_proba(user_data_encoded)[0][1] * 100
+
+    disease_risk = {
+        'Arthritis': risk_percentage_A,
+        'Cancer': risk_percentage_C,
+        'Diabetes': risk_percentage_D,
+        'Heart Disease': risk_percentage_HD
+    }
+
+    return disease_risk
+
 
     
-    modelHD = joblib.load('models/random_forest_model_HD,pkl')
+    
+   
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from model import preprocess_and_predict
+from .model import predict_disease_risk
 
 bp = Blueprint('main', __name__)
 
@@ -7,7 +7,7 @@ bp = Blueprint('main', __name__)
 def index():
     if request.method == "POST":
         # Collect data from form
-        age_input = (request.form.get('age'))
+        age = (request.form.get('age'))
         gender = request.form.get('gender')       
         weight_input = (request.form.get('weight'))     
         height_input = (request.form.get('height'))      
@@ -18,13 +18,8 @@ def index():
 
         # Error handling 
         errors = []
-        if not age_input:
+        if not age:
             errors.append('Please fill in your age!')
-        else:
-            try:
-                age = int(age_input)
-            except ValueError:
-                errors.append('Please enter a valid integer for age!')
 
         if not gender:
             errors.append('Please select your gender!')
@@ -69,7 +64,7 @@ def index():
             }
                 
         # Pass data to the model to predict the disease risk
-        prediction = preprocess_and_predict(user_data)
+        disease_risk = predict_disease_risk(user_data)
 
-        return render_template('index.html', prediction=prediction)
+        return render_template('results.html', predictions=disease_risk)
     return render_template('index.html') 
